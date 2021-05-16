5b034a3e4b032b9e20ac794110edbbfb9bbd45a3
hyeoneedyou <hyeoneedyou@naver.com>
2021-05-16 16:57:55 +0900

    feat: Add templates

--- ideathon/views.py
+++ ideathon/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    return render(request, 'ideathon/main.html')
+
+def single_post(request):
+    return render(request, 'ideathon/single-post.html')
