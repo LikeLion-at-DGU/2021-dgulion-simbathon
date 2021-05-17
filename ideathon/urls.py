0a7182ced1f77f23a99b52e98711da14638e97d2
hyeoneedyou <hyeoneedyou@naver.com>
2021-05-18 00:25:35 +0900

    feat: Add create

--- ideathon/urls.py
+++ ideathon/urls.py
@@ -7,5 +7,7 @@ app_name='ideathon'
 urlpatterns =[
     path('', views.main, name="main"),
     path('single_post/', views.single_post, name="single_post"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ideathon/views.py
+++ ideathon/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post
 
 def main(request):
     return render(request, 'ideathon/main.html')
 
 def single_post(request):
     return render(request, 'ideathon/single-post.html')
+
+
+def new(request):
+    return render(request, 'ideathon/new.html')
+    
+def create(request):
+    new_post = Post()
+    new_post.title = request.POST['title']
+    new_post.category = request.POST['category']
+    new_post.content = request.POST['content']
+    new_post.medaifile = request.POST['mediafile']
+    new_post.save()
+    return redirect('ideathon:main')  
