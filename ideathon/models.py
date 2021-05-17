9dc4092f08316b84fd8bd7bee9a8f4a96ebc71ea
hyeoneedyou <hyeoneedyou@naver.com>
2021-05-17 23:14:58 +0900

    feat: Add Post model

--- ideathon/models.py
+++ ideathon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    POST_CATEGORY_CHOICES = [
+        ('hi-sw', 'hi-sw'),
+        ('club', 'club'),    
+    ]
+
+    category = models.CharField(choices=POST_CATEGORY_CHOICES, max_length=300)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    mediafile = models.FileField(upload_to='mediafiles/')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- ideathon/urls.py
+++ ideathon/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name='ideathon'
 urlpatterns =[
     path('', views.main, name="main"),
-    path('/single_post', views.single_post, name="single_post"),
+    path('single_post/', views.single_post, name="single_post"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
