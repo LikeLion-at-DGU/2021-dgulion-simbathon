952f57529da41669d2c8015a0c14238b228f4dec
zoonong <zoonongzung@naver.com>
2021-05-20 01:45:32 +0900

    first commit about mainpage

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,10 @@ urlpatterns =[
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
     path('single_project/', views.single_project, name="single_project"),
+    path('simbathon/', views.simbathon, name="simbathon"),
+    path('process/', views.process, name="process"),
+    path('judge/', views.judge, name="judge"),
+    path('reward/', views.reward, name="reward"),
+    path('developer/', views.developer, name="developer"),
+    path('dgulion/', views.dgulion, name="dgulion"),
 ]
