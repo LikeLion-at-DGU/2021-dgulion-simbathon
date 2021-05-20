27c7ce824f79b597513bf01a7a4524631234f460
hyeoneedyou <hyeoneedyou@naver.com>
2021-05-20 20:45:18 +0900

    feat: Set mediafile null true

--- ideathon/models.py
+++ ideathon/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     category = models.CharField(choices=POST_CATEGORY_CHOICES, max_length=300)
     title = models.CharField(max_length=50)
     content = models.TextField()
-    mediafile = models.FileField(upload_to='mediafiles/', blank=True)
+    mediafile = models.FileField(upload_to='mediafiles/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
