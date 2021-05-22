5c0c9f3886a9ec1a898cb1b752f6612d4eac3174
dddooo9 <dlaehdus99@naver.com>
2021-05-23 00:54:37 +0900

    feat:Add comment in mypage, fix style

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from ideathon.models import Post
+from ideathon.models import Post, Comment
 
 
 def mypage(request):
   user = request.user
   posts = Post.objects.filter(writer=user)
-  return render(request, 'users/mypage.html', {'user':user, 'posts':posts})
+  comments = Comment.objects.filter(writer=user)
+  return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments})
