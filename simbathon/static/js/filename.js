62f68105a18c29678e30f57e696f14d72e6674a2
hyeoneedyou <hyeoneedyou@naver.com>
2021-05-29 06:57:44 +0900

    fix: Delte test file

--- simbathon/static/js/filename.js
+++
@@ -1,5 +0,0 @@
-var mediafile =  document.getElementById("onlyFileName").innerHTML;
-(function deleteParentdir(){
-    var mediaArray = mediafile.split('/');
-    document.getElementById("onlyFileName").innerHTML = mediaArray[1];
-})()
