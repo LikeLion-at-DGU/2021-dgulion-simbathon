c0b290b888c91277d19a9995c8b4f3f104d05755
hyeoneedyou <hyeoneedyou@naver.com>
2021-06-23 14:49:26 +0900

    feat: Change hackerthon description and delete unnecessary files

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name='main'
 urlpatterns =[
     path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
-    path('single_project/', views.single_project, name="single_project"),
     path('simbathon/', views.simbathon, name="simbathon"),
     path('process/', views.process, name="process"),
     path('judge/', views.judge, name="judge"),
