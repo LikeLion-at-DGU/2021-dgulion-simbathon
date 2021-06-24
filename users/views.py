693c60aad97954e235c2d07c849014cdc26e4221
dddooo9 <dlaehdus99@naver.com>
2021-06-24 14:53:57 +0900

    mypage에 해커톤 글작성 목록 추가

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from ideathon.models import Post, Comment
+from hackerthon.models import Result
 
 
 def mypage(request):
   user = request.user
   posts = Post.objects.filter(writer=user)
   comments = Comment.objects.filter(writer=user)
-  return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments})
+  results = Result.objects.filter(writer=user)
+  return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments, 'results':results})
