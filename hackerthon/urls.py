1f450002ef8dd8a7aaf2751896634f6307747592
dddooo9 <dlaehdus99@naver.com>
2021-06-24 17:05:46 +0900

    member 모델 추가 및 마이페이지에 처리

--- hackerthon/urls.py
+++ hackerthon/urls.py
@@ -10,4 +10,7 @@ urlpatterns =[
     path('<int:id>/', views.detail, name="detail"),
     path('update/<int:id>/', views.update, name="update"),
     path('delete/<int:id>/', views.delete, name="delete"),
+    #member
+    path('<int:id>/create_member', views.create_member, name="create_member"),
+    path('<int:id>/<int:member_id>/delete_member', views.delete_member, name="delete_member"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from ideathon.models import Post, Comment
-from hackerthon.models import Result
+from hackerthon.models import *
 
 
 def mypage(request):
   user = request.user
   posts = Post.objects.filter(writer=user)
   comments = Comment.objects.filter(writer=user)
-  results = Result.objects.filter(writer=user)
-  return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments, 'results':results})
+  try:
+    member = Member.objects.get(member=user.last_name + user.first_name)
+    return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments, 'member':member})
+  except Member.DoesNotExist:
+    return render(request, 'users/mypage.html', {'user':user, 'posts':posts, 'comments':comments})
